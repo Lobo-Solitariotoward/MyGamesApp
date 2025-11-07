02e220292010551483c68f9673ae4c57cd219afb
andr3s005 <asg78886@lasallebajio.edu.mx>
2025-11-06 19:11:38 -0600

    Creacion y modificacion de HomeViewModel

---
+++ app/src/main/java/com/example/videogamesapi/screens/viewmodel/HomeViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.videogamesapi.screens.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.videogamesapi.data.MyGamesRepository
+import com.example.videogamesapi.models.Category
+import com.example.videogamesapi.models.Games
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class HomeViewModel(
+    private val repository: MyGamesRepository = MyGamesRepository()
+) : ViewModel() {
+    private val _categories = MutableStateFlow<List<Category>>(emptyList())
+    val categories: StateFlow<List<Category>> = _categories
+
+    private val _trendingGames = MutableStateFlow<List<Games>>(emptyList())
+    val trendingGames: MutableStateFlow<List<Games>> = _trendingGames
+
+    private val _newGames = MutableStateFlow<List<Games>>(emptyList())
+    val newGames: MutableStateFlow<List<Games>> = _newGames
+
+    init {
+        loadHomeData()
+    }
+
+    private fun loadHomeData(){
+        viewModelScope.launch {
+            _categories.value = repository.getCategories()
+            _trendingGames.value = repository.getTrendingGames()
+            _newGames.value = repository.getNewGames()
+        }
+    }
+
+    fun selectCategory(categoryId : Int) {
+        val updatedList = _categories.value.map { category ->
+            category.copy(isSelected = category.id == categoryId)
+        }
+        _categories.value = updatedList
+    }
+}
