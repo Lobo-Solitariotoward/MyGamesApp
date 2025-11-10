f1050c571134755de1be2c1419bd43dc5e0a9549
andr3s005 <asg78886@lasallebajio.edu.mx>
2025-11-10 10:56:35 -0600

    Agregamos la data class del GamesDetail en su archivo

--- app/src/main/java/com/example/videogamesapi/models/GamesDetail.kt
+++ app/src/main/java/com/example/videogamesapi/models/GamesDetail.kt
@@ -1,2 +1,27 @@
 package com.example.videogamesapi.models
 
+data class GamesDetail (
+    val id: Int,
+    val title: String,
+    val category: String,
+    val headerImageUrl: String,
+    val avatarImageUrl: String,
+    val description: String,
+    val followers: String,
+    val players: String,
+    val streamers: String,
+    val liveStreams: List<LiveStream>,
+    val playersOnline: List<PlayerOnline>
+)
+
+data class LiveStream(
+    val title: String,
+    val user: String,
+    val viewerCount: String,
+    val streamImageUrl: String
+)
+
+data class PlayerOnline(
+    val username: String,
+    val avatarUrl: String
+)
