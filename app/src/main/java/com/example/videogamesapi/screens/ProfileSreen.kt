13a6e12d2acbdb10a1dd4d18c50aa32fa896de26
Lobo-Solitariotoward <ariciagamax7@gmail.com>
2025-11-09 22:07:26 -0600

    Agrego imagen de portada e iconos de notificación y menú

--- app/src/main/java/com/example/videogamesapi/screens/ProfileSreen.kt
+++ app/src/main/java/com/example/videogamesapi/screens/ProfileSreen.kt
@@ -1,12 +1,20 @@
 package com.example.videogamesapi.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.rememberAsyncImagePainter
 
 @Composable
 fun ProfileHeader() {
@@ -15,7 +23,37 @@ fun ProfileHeader() {
             .fillMaxSize()
             .background(Color(0xFF0C0F27))
     ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = rememberAsyncImagePainter(
+                    "https://img.goodfon.com/original/1920x1080/8/47/valorant-valorant-video-igry-takticheskii-shuter-riot-game-2.jpg"
+                ),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(250.dp),
+                contentScale = ContentScale.Crop
+            )
 
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(25.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Notifications,
+                    contentDescription = "Notificaciones",
+                    tint = Color.White
+                )
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = "Menú",
+                    tint = Color.White
+                )
+            }
+        }
     }
 }
 
