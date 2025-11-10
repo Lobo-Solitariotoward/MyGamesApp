5898201eb9cec33f89f6f5f29370a9d3f934e232
Lobo-Solitariotoward <ariciagamax7@gmail.com>
2025-11-09 23:43:10 -0600

    Modifique Routes Para Probar La Navegacion

--- app/src/main/java/com/example/videogamesapi/navigation/Routes.kt
+++ app/src/main/java/com/example/videogamesapi/navigation/Routes.kt
@@ -1,2 +1,11 @@
 package com.example.videogamesapi.navigation
 
+sealed class Routes(val route: String) {
+    object Home : Routes("home")
+    object Profile : Routes("profile")
+    object Trending : Routes("trending")
+    object Chat : Routes("chat")
+    object MyGames : Routes("my_games")
+    object Login : Routes("login")
+    object Register : Routes("register")
+}
