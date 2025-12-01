bafc86f0818190deb0bef451457485538bd84f23
Dairy-Mod <albert-0605@outlook.com>
2025-12-01 16:41:48 -0600

    Definir endpoints

--- app/src/main/java/com/example/videogamesapi/remote/GamesApi.kt
+++ app/src/main/java/com/example/videogamesapi/remote/GamesApi.kt
@@ -1,2 +1,17 @@
 package com.example.videogamesapi.remote
 
+import com.example.videogamesapi.models.Games
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GamesApi {
+
+    @GET("games/local")
+    suspend fun getAllGames(): List<Games>
+
+    @GET("games/local/id/{id}")
+    suspend fun getGameById(@Path("id") id: String): Games
+
+    @GET("games/local/search/{term}")
+    suspend fun searchGames(@Path("term") term: String): List<Games>
+}
