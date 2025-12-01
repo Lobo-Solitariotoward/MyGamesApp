1c6c78dfcb922ffd0d84428891e95c6b4393fc42
Dairy-Mod <albert-0605@outlook.com>
2025-12-01 16:44:53 -0600

    Retrofit con URL base

--- app/src/main/java/com/example/videogamesapi/remote/NetworkModule.kt
+++ app/src/main/java/com/example/videogamesapi/remote/NetworkModule.kt
@@ -1,2 +1,28 @@
 package com.example.videogamesapi.remote
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NetworkModule {
+
+    private const val BASE_URL = "http://10.0.2.2:3000/"
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    val gamesApi: GamesApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(GamesApi::class.java)
+    }
+}
