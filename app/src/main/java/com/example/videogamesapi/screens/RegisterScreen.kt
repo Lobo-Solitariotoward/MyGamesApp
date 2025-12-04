7e8cb2eb8550251cf32d90709945b73b8f5f9e2b
GerardoAV1 <gerardo_amezquita@outlook.com>
2025-12-04 13:27:47 -0600

    Estructura del RegisterScreen Parte 1

--- app/src/main/java/com/example/videogamesapi/screens/RegisterScreen.kt
+++ app/src/main/java/com/example/videogamesapi/screens/RegisterScreen.kt
@@ -1,2 +1,69 @@
 package com.example.videogamesapi.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RegisterScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.horizontalGradient(
+                    colors = listOf(
+                        Color(0xFF2C2D51),
+                        Color(0xFF4A5090)
+                    )
+                )
+            )
+
+    ){
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(210.dp)
+        ){
+
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 210.dp)
+                .clip(RoundedCornerShape(topStart = 50.dp, topEnd = 50.dp))
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(
+                            Color(0xFF373B5B),
+                            Color(0xFF323253)
+                        )
+                    )
+                )
+        ){
+
+        }
+
+    }
+
+}
+
+@Preview(showBackground = true,
+    showSystemUi = true)
+
+@Composable
+fun RegisterScreenPreview(){
+    RegisterScreen()
+}
+
+
