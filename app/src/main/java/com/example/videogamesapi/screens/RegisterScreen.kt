4894d495e190c55106d61ccd735b77ae9817b734
GerardoAV1 <gerardo_amezquita@outlook.com>
2025-12-04 13:34:18 -0600

    Título y la mitad de opciones del RegisterScreen Parte 2

--- app/src/main/java/com/example/videogamesapi/screens/RegisterScreen.kt
+++ app/src/main/java/com/example/videogamesapi/screens/RegisterScreen.kt
@@ -2,11 +2,19 @@ package com.example.videogamesapi.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -14,6 +22,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun RegisterScreen(){
@@ -51,6 +60,48 @@ fun RegisterScreen(){
                     )
                 )
         ){
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(all = 50.dp)
+
+            ) {
+                Text(text = "Sign Up",
+                    color = Color.White,
+                    fontSize = 35.sp
+
+                )
+                Spacer(modifier = Modifier.height(50.dp))
+
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    placeholder = {
+                        Text(text = "Username", color = Color.White)
+                    },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Default.AccountCircle,
+                            contentDescription = "Email",
+                            tint = Color(0xFFB0B0B0)
+                        )
+                    },
+                )
+                Spacer(modifier = Modifier.height(25.dp))
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    placeholder = {
+                        Text(text = "Email", color = Color.White)
+                    },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Default.Email,
+                            contentDescription = "Lock",
+                            tint = Color(0xFFB0B0B0)
+                        )
+                    },
+                )
+
+            }
 
         }
 
